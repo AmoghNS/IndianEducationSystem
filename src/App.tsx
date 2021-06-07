255c091480189f5b3d070589ca792eeb0dedca66
AmoghNS <80661813+AmoghNS@users.noreply.github.com>
2021-06-07 15:00:35 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//Sections
+import Base from "./Sections/Base"
+//import Footer from "./Sections/Main"
+
+function App() {
+  return (
+    <div>
+      <Base/>
+    </div>
+  );
+}
+
+export default App;
